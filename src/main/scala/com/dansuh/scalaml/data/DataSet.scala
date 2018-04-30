ded88cf1347d105e9d8e3438bdf2506c3ef1a9e3
Dan Suh <kaehops@gmail.com>
2018-04-30 18:37:34 +0900

    sequence

--- src/main/scala/com/dansuh/scalaml/data/DataSet.scala
+++ src/main/scala/com/dansuh/scalaml/data/DataSet.scala
@@ -5,6 +5,14 @@ trait LabeledData[LabelType, SampleType] extends Product2[LabelType, SampleType]
   def sample: SampleType = _2
 }
 
+object LabeledData {
+  def apply[A, B](l: A, s: B) = new LabeledData[A, B](l, s) {
+    override def _1: A = l
+    override def _2: B = s
+    override def canEqual(that: Any): Boolean = that.isInstanceOf[LabeledData[A, B]]
+  }
+}
+
 trait DataSet[Label, Sample] {
   // type alias for sequence of labeled data
   type DataSeqType = Seq[LabeledData[Label, Sample]]
@@ -24,7 +32,12 @@ trait DataSet[Label, Sample] {
   }
 }
 
-case class MnistData(data: LabeledData[Int, Seq[Int]])
+case class MnistData(_1: Int, _2: Seq[Int]) extends LabeledData[Int, Seq[Int]]
+case class MnistDataSet(data: Seq[LabeledData[Int, Seq[Int]]])
+  extends DataSet[Int, Seq[Int]]
 
 object DataSet {
+  def fromTupleSequence[A, B](rawData: Seq[(A, B)]): DataSet[A, B] = new DataSet[A, B] {
+    def data: Seq[LabeledData[A, B]] = rawData.map(a => LabeledData(a._1, a._2))
+  }
 }
